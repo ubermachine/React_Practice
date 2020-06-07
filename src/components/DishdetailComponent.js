914465e9415e092fd3ebfad94ba278b8d9ac7d06
ubermachine <shivamkatoch@outlook.com>
2020-06-08 02:04:26 +0530

    staged part 1

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
+
+class DishDetail extends Component {
+  renderDish(dish) {
+    return (
+      <div className="col=12 col-md-5 m-1">
+        <Card>
+          <CardImg width="100%" src={dish.image} alt={dish.name} />
+          <CardBody>
+            <CardTitle>{dish.name}</CardTitle>
+            <CardText>{dish.description}</CardText>
+          </CardBody>
+        </Card>
+      </div>
+    );
+  }
+
+  renderComments(comments) {
+    const commentlist = comments.map((comment) => {
+      return (
+        <li key={comment.id}>
+          <p>{comment.comment}</p>
+          <p>
+            --{comment.author},{" "}
+            {new Intl.DateTimeFormat("en-US", {
+              year: "numeric",
+              month: "short",
+              day: "2-digit",
+            }).format(new Date(Date.parse(comment.date)))}
+          </p>
+        </li>
+      );
+    });
+
+    return commentlist;
+  }
+
+  render() {
+    if (this.props.dish != null) {
+      return (
+        <div class="container">
+          <div className="row">
+            {this.renderDish(this.props.dish)}
+            <div className="col=12 col-md-5 m-1">
+              <h4>Comments</h4>
+              <ul className="list-unstyled">
+                {this.renderComments(this.props.dish.comments)}
+              </ul>
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return <div></div>;
+    }
+  }
+}
+
+export default DishDetail;

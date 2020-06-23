8bbe8c6c89e933ed0cd06c444262ad92287d7d9e
ubermachine <shivamkatoch@outlook.com>
2020-06-23 12:37:10 +0530

    post comment using fetch

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -39,7 +39,7 @@ class CommentForm extends Component {
   }
   handleSubmit(values) {
     this.toggleModal();
-    this.props.addComment(
+    this.props.postComment(
       this.props.dishId,
       values.rating,
       values.author,
@@ -61,10 +61,10 @@ class CommentForm extends Component {
               onSubmit={(values) => this.handleSubmit(values)}
             >
               <Row className="form-group">
-                <Label htmlFor="yourName">Rating</Label>
+                <Label htmlFor="rating">Rating</Label>
                 <Control.select
-                  model=".contactType"
-                  name="contactType"
+                  model=".rating"
+                  name="rating"
                   className="form-control"
                 >
                   <option>1</option>
@@ -144,7 +144,7 @@ function RenderDish({ dish }) {
     </div>
   );
 }
-function RenderComments({ comments, addComment, dishId }) {
+function RenderComments({ comments, postComment, dishId }) {
   //if (comments != null)
   return (
     <div className="col-12 col-md-5 m-1">
@@ -166,7 +166,7 @@ function RenderComments({ comments, addComment, dishId }) {
           );
         })}
       </ul>
-      <CommentForm dishId={dishId} addComment={addComment} />
+      <CommentForm dishId={dishId} postComment={postComment} />
     </div>
   );
 }
@@ -207,7 +207,7 @@ const DishDetail = (props) => {
 
           <RenderComments
             comments={props.comments}
-            addComment={props.addComment}
+            postComment={props.postComment}
             dishId={props.dish.id}
           />
         </div>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -11,9 +11,8 @@ import { Switch, Route, Redirect, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 
 import { actions } from "react-redux-form";
-
 import {
-  addComment,
+  postComment,
   fetchDishes,
   fetchComments,
   fetchPromos,
@@ -29,8 +28,8 @@ const mapStateToProps = (state) => {
 };
 
 const mapDispatchToProps = (dispatch) => ({
-  addComment: (dishId, rating, author, comment) =>
-    dispatch(addComment(dishId, rating, author, comment)),
+  postComment: (dishId, rating, author, comment) =>
+    dispatch(postComment(dishId, rating, author, comment)),
   fetchDishes: () => {
     dispatch(fetchDishes());
   },
@@ -83,7 +82,7 @@ class Main extends Component {
             (comment) => comment.dishId === parseInt(match.params.dishId, 10)
           )}
           commentsErrMess={this.props.comments.errMess}
-          addComment={this.props.addComment}
+          postComment={this.props.postComment}
         />
       );
     };
